a5ce76badcde60b47ddd14f191a991d9d6a79b3e
Marion <wekesamarion100@gmail.com>
2024-10-29 19:06:28 +0300

    complete lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,11 @@
+import { render } from "@testing-library/react";
 import React from "react";
+
 
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test 
     </div>
   );
 }
